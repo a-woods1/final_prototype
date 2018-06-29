5fc6d6d1c29f0b74f91a8d7126fdda94f99e30c7
a-woods1 <seiadmin@Aubries-MacBook-Pro.local>
2018-06-29 15:24:01 -0400

    updated database connection info

--- app/auth/views.py
+++ app/auth/views.py
@@ -44,7 +44,7 @@ def load_user(id):
 def register():
   form = RegistrationForm()
   if form.validate_on_submit():
-    user = User(email=form.email.data, username=form.username.data, first_name=form.first_name.data, last_name=form.last_name.data, password=form.password.data)
+    user = User(id=1, email=form.email.data, username=form.username.data, first_name=form.first_name.data, last_name=form.last_name.data, password=form.password.data)
     # add user to the database
     db.session.add(user)
     db.session.commit()
--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,2 @@
 #SQLALCHEMY_DATABASE_URI = "postgresql://##USER##:##PASSWORD##@##HOST##/prototype"
+SQLALCHEMY_DATABASE_URI = "postgresql://bb_admin:welcome123@localhost:5432/prototype_db"
